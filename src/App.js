a2f664a27062f7ebec73b12cfcf817cd0615aae9
octavio masdeu <masdeuocta@gmail.com>
2021-11-09 19:51:25 -0300

    parrafo nuevo y link a github

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Mi primer proyecto React.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Octavio-dev/proyectoReact-Masdeu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          tocá acá
         </a>
       </header>
     </div>
