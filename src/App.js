6f88b96f7711a5fe9f9ce79c98d76b28d84e8df2
octavio masdeu <masdeuocta@gmail.com>
2021-11-11 19:57:00 -0300

    components/headers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import react, {Component} from "react";
 import "./App.css"
 
+//Components
+import Header from "./components/Header/Header"
+
 //render() solo se usa en class
 
 class App extends Component {
   render() {
     return (
-      <div className = "header">
-        <h1>Hola crack</h1>
-        <h2>Que onda</h2>
+      <div>
+        <Header />
       </div>
     )
   }
