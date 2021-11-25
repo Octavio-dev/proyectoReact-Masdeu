8a6433444ec88a4e86e247ff9e9362f14482cbf8
octavio masdeu <masdeuocta@gmail.com>
2021-11-25 18:25:42 -0300

    emulado retraso de red con setTimeOut

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,20 +3,18 @@ import React, {useEffect, useState} from "react";
 //componentes
 import Item from "../Item/Item"
 
-//imagenes provisionales
-import lesPaul01 from "./lesPaul.png"
-import lesPaul02 from "./lesPaul02.jpg"
-import lesPaul03 from "./lesPaul03.jpg"
-import violin from "./violin02.png"
-
 const ItemList = () => {
 
     const [productos, setProductos] = useState([])
 
     useEffect( () => {
-        fetch("productos.json")
-        .then((response) => response.json())
-        .then((json) => setProductos(json))
+        
+        setTimeout(() => {
+
+            fetch("productos.json")
+            .then((response) => response.json())
+            .then((json) => setProductos(json))
+        },2000)
     },[])
 
     return(
