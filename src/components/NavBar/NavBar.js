59e901ce50700a1ec27aa2b27be602a41449d3cb
octavio masdeu <masdeuocta@gmail.com>
2021-12-02 23:23:13 -0300

    rutas estaticas a la navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,12 +15,12 @@ function NavBar ({a1,a2,a3,a4,a5, a6}) {
     return(
         <div className="navContenedor">
             <ul className="navBar">
-                <Link className="Link" to="/">{a1}</Link>
-                <Link className="Link" to="/">{a2}</Link>
-                <Link className="Link" to="/">{a3}</Link>
-                <Link className="Link" to="/">{a4}</Link>
-                <Link className="Link" to="/">{a5}</Link>
-                <Link className="Link" to="/">{a6}</Link>
+                <Link className="Link" to="/Fender">{a1}</Link>
+                <Link className="Link" to="/Yamaha">{a2}</Link>
+                <Link className="Link" to="/Jackson">{a3}</Link>
+                <Link className="Link" to="/Epiphone">{a4}</Link>
+                <Link className="Link" to="/PRS">{a5}</Link>
+                <Link className="Link" to="/Ibanez">{a6}</Link>
             </ul>
             <CartWidjet />
         </div>
