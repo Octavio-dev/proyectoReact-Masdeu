1a84dd4fa62b329b22d41b84bc471a4c7ec71070
octavio masdeu <masdeuocta@gmail.com>
2021-12-03 16:57:34 -0300

    navBar con links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,12 +15,12 @@ function NavBar ({a1,a2,a3,a4,a5, a6}) {
     return(
         <div className="navContenedor">
             <ul className="navBar">
-                <Link className="Link" to="/categoryFender">{a1}</Link>
-                <Link className="Link" to="/categoryYamaha">{a2}</Link>
-                <Link className="Link" to="/categoryJackson">{a3}</Link>
-                <Link className="Link" to="/categoryEpiphone">{a4}</Link>
-                <Link className="Link" to="/categoryPRS">{a5}</Link>
-                <Link className="Link" to="/categoryIbanez">{a6}</Link>
+                <Link className="Link" to="/category">{a1}</Link>
+                <Link className="Link" to="/category">{a2}</Link>
+                <Link className="Link" to="/category">{a3}</Link>
+                <Link className="Link" to="/category">{a4}</Link>
+                <Link className="Link" to="/category">{a5}</Link>
+                <Link className="Link" to="/category">{a6}</Link>
             </ul>
             <CartWidjet />
         </div>
