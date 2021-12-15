9ba9f315adbb8d4bfbc2a7ceab3c840204fc6d94
octavio masdeu <masdeuocta@gmail.com>
2021-12-14 21:34:39 -0300

    contador de productos al cartWidget todavia no se renderiza

--- src/components/NavBar/cartWidjet.js
+++ src/components/NavBar/cartWidjet.js
@@ -6,12 +6,17 @@ import { Link } from "react-router-dom"
 //SEMANTIC UI
 import {Icon} from "semantic-ui-react"
 
+import {carro} from "../../cartContext"
+
 function CartWidjet() {
+
+    const totalProd = carro.reduce( (acc, prod) => acc += prod.cantidad, 0)
 
     return(
            <Link to="/cart">
                 <button className="cart">
                     <Icon className="iconCart" name = "shopping cart" color ="yellow" size="big"/>
+                    <small>{totalProd}</small>
                 </button>
            </Link>
     )
